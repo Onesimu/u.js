2cc39e69a89ba7f719bf6d2dc85b9ce78ed04096
onesimu <872634214@qq.com>
2021-09-28 16:30:27 +0800

    fix

--- dev/qht.js
+++ dev/qht.js
@@ -5,41 +5,28 @@ const qht = e => {
   const l = e.t().t('\n').n(i => i.t()).t(i => i.t(/^\s*/)[0].i() / 2 + i.t())
   o(l)
 
-  const a = []
-  a.e(l[0])
-  // for (var i of u.n(l.i()).t(1, -1)) {
-  //   const t = l[i];
-  //   const e = t[0];
-  //   const c = l[i + 1][0] - e
-  //   if (c == 1) a.e([t])
-  //   if (c == 0) a.i(-1).e([t])
-  //   if (c == -1) a.i(-1).e([t])
-  // }
+  const a = [l[0]]
 
   var pmt = [a]
-  var tmp = a
   var pi = 0
+  var tmp = a
   for(var x = l.i(), i = 1; i < x; i++) {
       const t = l[i]
       const c = t[0] - l[i - 1][0]
       const tp = [t]
       if (c == 1) {
-        // if (i > 2 && e - l[i - 2][0] == 2) pmt = tmp
         pmt.e(tmp)
         pi++
         tmp.e(tp)
-      } else
-      if (c == 0) {
+      } else if (c == 0) {
         a.i(-1).e(tp)
-      } else
-      if (c == -1) {
-        pi--
+      } else if (c < 0) {
+        pi += c
         pmt[pi].e(tp)
       }
       tmp = tp
-    o(c, u.t(a), pmt, tmp)
+    // o(c, u.t(a), pmt, tmp)
   }
-  // a.i(-1).e([l.i(-1)])
 
   const tl = u.t(a).e(/"\d/g, '"')
   o(u.t(a), a, tl)
