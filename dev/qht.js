bcb01ae814b1e9671000fb4a305dbab097be27cd
onesimu <872634214@qq.com>
2021-09-28 16:01:27 +0800

    ht

--- dev/qht.js
+++ dev/qht.js
@@ -7,22 +7,49 @@ const qht = e => {
 
   const a = []
   a.e(l[0])
-  for (var i of u.n(l.i()).t(1, -1)) {
-    // for(var x = l.i(), i = 1; i < x - 1; i++){
-    // if (i == l.i() - 1) {a.i(-1).e([l[i]]); break}
-    // if (i == 0) {a.e(l[i]); break}
+  // for (var i of u.n(l.i()).t(1, -1)) {
+  //   const t = l[i];
+  //   const e = t[0];
+  //   const c = l[i + 1][0] - e
+  //   if (c == 1) a.e([t])
+  //   if (c == 0) a.i(-1).e([t])
+  //   if (c == -1) a.i(-1).e([t])
+  // }
 
-    const t = l[i];
-    const e = t[0];
-    const c = l[i + 1][0] - e
-    if (c == 1) a.e([t])
-    if (c == 0) a.i(-1).e([t])
-    if (c == -1) a.i(-1).e([t])
+  var pmt = a
+  var tmp = a
+  for(var x = l.i(), i = 1; i < x; i++) {
+    // if (i == 0) {a.e(l[i])}
+    // else if (i == x - 1) {a.i(-1).e([l[i]])}
+    // else {
+      const t = l[i];
+      const e = t[0];
+      const c = e - l[i - 1][0]
+      // if (c == 1) { tmp = [t]; a.e(tmp)}
+      if (c == 1) {
+        if (i > 2 && e - l[i - 2][0] == 2) pmt = tmp
+        const tp = [t]
+        tmp.e(tp)
+        tmp = tp
+      } else
+      if (c == 0) {
+        const tp = [t]
+        a.i(-1).e(tp)
+        tmp = tp
+      } else
+      if (c == -1) {
+        // pmt = tmp
+        const tp = [t]
+        pmt.e(tp)
+        tmp = tp
+      }
+    // }
+    o(c, u.t(a), pmt, tmp)
   }
-  a.i(-1).e([l.i(-1)])
+  // a.i(-1).e([l.i(-1)])
 
   const tl = u.t(a).e(/"\d/g, '"')
-  o(a, tl)
+  o(u.t(a), a, tl)
   // const dt = { '[': '(', ']': ')' }
   // const dt = { '[': '{', ']': '}' }
   const dt = {
@@ -30,11 +57,12 @@ const qht = e => {
     '\\]': '</div>',
     '\\"': '',
     '\\,': '',
-    '<div>((\\w)\\d+)': '<div id="$1" class="$2 $1">',
+    '<div>((\\w)\\d+)': '<div id="$1">',
     '>\\s': '>'
   }
 
-  const h = tl.e(dt).e(/<div>(.+?)</, '<div id="$1" class="$1"><')
+  const h = tl.e(dt)
+  // .e(/<div>(.+?)</, '<div id="$1" class="$1"><')
   o(h)
   return h
 }
