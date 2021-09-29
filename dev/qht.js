4a5f8126cf192efaef5f979824af4fa4449fdda5
onesimu <872634214@qq.com>
2021-09-29 08:51:45 +0800

    fix

--- dev/qht.js
+++ dev/qht.js
@@ -2,14 +2,16 @@ const qht = e => {
   const id = /\.([a-z]\d+)\s/g
   if (id.test(e)) return e.e(/\.(f\d+)\s/g, '<dd id=$1 class=$1>').e(/\.(e\d+)\s/g, '<dl id=$1 class=$1>\n').e(/^\//gm, '</dl>')
   if(e.startsWith('<', 0)) return e
-  const l = e.t().t('\n').n(i => i.t()).t(i => i.t(/^\s*/)[0].i() / 2 + i.t())
+  const l = e.t().t('\n').n(i => i.t())
+    .t((i, t) => i.t(/^\s*/)[0].i() / 2 + 'f' + t)
+    // .t(i => i.t(/^\s*/)[0].i() / 2 + i.t())
   o(l)
 
   const a = [l[0]]
 
-  var pmt = [a] // parent list
-  var pi = 0 // parent index
-  var tmp = a //
+  var pmt = [] // parent list
+  var pi = -1 // parent index
+  var tmp = a // potential parent
   for(var x = l.i(), i = 1; i < x; i++) {
       const t = l[i]
       const tp = [t]
@@ -19,13 +21,13 @@ const qht = e => {
         pi++
         tmp.e(tp)
       } else if (c == 0) {
-        a.i(-1).e(tp)
+        pmt.i(-1).e(tp)
       } else if (c < 0) {
         pi += c
         pmt[pi].e(tp)
       }
       tmp = tp
-    // o(c, u.t(a), pmt, tmp)
+    o(c, pmt, tmp, pi, pmt[pi], u.t(a))
   }
 
   const tl = u.t(a).e(/"\d/g, '"')
