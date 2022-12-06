a1546a2a737d9101234bf65e4fa2bd94793dec35
onesimu <872634214@qq.com>
2022-12-06 15:39:20 +0800

    fix

--- dev/qls.js
+++ dev/qls.js
@@ -1,4 +1,10 @@
 String.prototype.e = String.prototype.replace
-const srct = it => it.e(/^\s*[\/]{2}/gm, '#').e(/\$/gm, '#').e(/^(\s*if .+?) rn\s*/gm, '$1 then return ').e(/\brn\b/gm, 'return')
-      .e(/^\s*[\/]{2}/gm, '#').e(/^\s*(import .+)$/gm, '``$1``\n').e(/^\s*(export default)/gm, '``$1``')
-const dett = it => it.e(/;$/gm, '').e(/function\s*\((.*)\)/g, '($1) => ')      
+const srct = it => it.e(/^\s*[\/]{2}/gm, '#')
+.e(/".*?\$.*?"/gm, i => i.e('\\$', '#'))
+.e(/^(\s*if .+?) rn\s*/gm, '$1 then return ')
+.e(/\brn\b/gm, 'return')
+.e(/^\s*(import .+)$/gm, '``$1``\n').e(/^\s*(export default)/gm, '``$1``')
+            
+const dett = it => it.e(/;$/gm, '').e(/function\s*\((.*)\)/g, '($1) => ')
+
+export { srct, dett }      
