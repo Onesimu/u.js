7b37cb91f2d06292971e079606525d7d4fdffdf3
onesimu <872634214@qq.com>
2023-02-20 17:35:09 +0800

    clean

--- dev/q.js
+++ dev/q.js
@@ -25,7 +25,8 @@ const init = async () => {
     u.qcs = r.qcs
     u.bd.n('style').e(it => it.h(u.qcs))
     window.ls = require("livescript")
-    window.onload = function(){ ls.go() }
+    // window.onload = function(){ ls.go() }
+    ls.go()
   })
 }
 scr('https://static-mp-12b08abd-474c-42c9-8737-5f9e0fdc300a.next.bspapp.com/lib/u.js', init)
